e7af39220793898b4549e9f05ecf24eaa1a0548f
Quiddale <qosullivan962@gmail.com>
2017-09-04 21:08:42 +0100

    pokemonapi

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do
-  resources :worlds
+Rails.application.routes.draw do  
   scope :api do
+    resources :worlds
     resources :users
     post "/register", to: "authentications#register"
     post "/login", to: "authentications#login"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,4 @@ u1 = User.create!(username: "Bob", email: "bob@gmail.com", password: "password",
 u2 = User.create!(username: "james", email: "james@gmail.com", password: "password", password_confirmation: "password" )
 u3 = User.create!(username: "hugo", email: "hugo@gmail.com", password: "password", password_confirmation: "password" )
 
-w1 = World.create!(title: "house", image: "/Users/quiddaleosullivan/Development/WDI_PROJECT4/WDI_PROJECT4_CLIENT/src/images/House_1.jpg")
-w2 = World.create!(title: "mountin", image: "/Users/quiddaleosullivan/Development/WDI_PROJECT4/WDI_PROJECT4_CLIENT/src/images/Mountin_1.jpg")
-w3 = World.create!(title: "space_station", image: "/Users/quiddaleosullivan/Development/WDI_PROJECT4/WDI_PROJECT4_CLIENT/src/images/Station_1.jpg")
-w4 = World.create!(title: "triangle", image: "/Users/quiddaleosullivan/Development/WDI_PROJECT4/WDI_PROJECT4_CLIENT/src/images/Triangle_1.jpg")
+w1 = World.create!(title: "triangle", image: "https://imgur.com/a/RmsVl")
